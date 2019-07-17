7434bc373b474a2132a2853797de9c704e8b8c47
Barbara Johnson <johnson.barbara@gmail.com>
2019-07-17 02:56:08 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
     root 'products#index'
+    #homepage adds items to cart when submitted
     post '/products' => 'products#add'
 
 end
