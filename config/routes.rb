3a252b638f594f8681d9af95cdee3467c7457e58
Barbara Johnson <johnson.barbara@gmail.com>
2019-07-17 03:39:34 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
-  root :to => "products#index"
+  root 'products#index'
   
   #call add method to add item to cart when form is submitted
   post :products, :to => "products#add"
